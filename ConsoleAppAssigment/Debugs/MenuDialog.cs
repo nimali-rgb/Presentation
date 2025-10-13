90437e6fca1f7a187b4fe9b566662f9b723c1fb5
Nima <nima010607@gmail.com>
2025-10-13 11:50:29 +0200

    ProductDialog and MenuDialog completed.

--- ConsoleAppAssigment/Debugs/MenuDialog.cs
+++ ConsoleAppAssigment/Debugs/MenuDialog.cs
@@ -2,7 +2,38 @@
 
 namespace ConsoleAppAssigment.Debugs;
 
-public class MenuDialog
+public class MenuDialog(ProductDialog productDialog)
 {
-  
+    private readonly ProductDialog _productDialog = productDialog;
+
+    public void MenuOptionDialog()
+    {
+        Console.Clear();
+        Console.WriteLine("-----New Menu Option-----");
+        Console.WriteLine(" 1. View Product List ");
+        Console.WriteLine(" 2. New Product ");
+
+        Console.Write(" Chosse A Menu Option ");
+        var option = Console.ReadLine();
+
+        switch (option)
+        {
+            case "1":
+                _productDialog.ListProductDialog();
+
+                break;
+
+            case "2":
+                _productDialog.ListProductDialog();
+                break;
+        }
+    }
+
+
+    public void Run()
+    {
+        while(true) 
+        {
+            MenuOptionDialog();
+        }
 }
--- ConsoleAppAssigment/Debugs/ProductDialog.cs
+++ ConsoleAppAssigment/Debugs/ProductDialog.cs
@@ -25,4 +25,23 @@ public class ProductDialog (IProductService productService)
         _productService.AddProductToList(product);
     }
 
+
+    public void ListProductDialog()
+    {
+        var products = _productService.GetProductFromList();
+
+        Console.Clear();
+        Console.WriteLine("  -New Prodect-  ");
+
+
+        foreach (var product in products)
+        {
+            Console.WriteLine(" Id: " + product.Id);
+            Console.WriteLine(" Name: " + product.Name);
+            Console.WriteLine(" Price: " + product.Price + "SEK");
+        }
+
+        Console.WriteLine(" Press Any Button...");
+        Console.ReadKey();
+    }
 }
