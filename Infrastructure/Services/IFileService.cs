a4a9c22062c456a256be3fde1429d4d802317d87
Nima <nima010607@gmail.com>
2025-10-13 09:49:10 +0200

    made ProductService and IFileService

---
+++ Infrastructure/Services/IFileService.cs
@@ -0,0 +1,10 @@
+﻿using Infrastructure.Models;
+
+namespace Infrastructure.Services;
+
+public interface IFileService
+{
+    void Initalizer();
+    IEnumerable<Product> ReadFromFile();
+    void WriteToFile(IEnumerable<Product> products);
+}
---
+++ Infrastructure/Services/ProductService.cs
@@ -0,0 +1,26 @@
+﻿
+using Infrastructure.Models;
+
+namespace Infrastructure.Services;
+
+public class ProductService
+{
+    private List<Product> _products = [];
+    private readonly IFileService _fileService = fileService
+
+        public void AddProductToList(Product, product)
+    {
+        Product.Id = Guid.NewGuid.ToString();
+
+        _products.Add(product);
+
+        _fileService.WriteToFile(_products);
+    }
+
+    public IEnumerable<Product> GetProductFromList()
+    {
+        _products = [.. _fileService.ReadFromFile()];
+        return _products.OrderBy(product => product.Name);
+    }
+
+}
