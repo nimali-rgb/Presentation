5229617da1e142a3781896eb0570eadc0868440f
Nima <nima010607@gmail.com>
2025-10-13 09:30:16 +0200

    Add project files.

---
+++ Infrastructure/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Infrastructure.Models;
+
+public class Product
+{
+    public string Id { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; } 
+}
