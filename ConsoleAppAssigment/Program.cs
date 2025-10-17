9f9e0d9bf1dbcf34a234db2329f18435aacb244b
Nima <nima010607@gmail.com>
2025-10-17 14:44:06 +0200

    ConsoleApp completed

--- ConsoleAppAssigment/Program.cs
+++ ConsoleAppAssigment/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ConsoleAppAssigment.Debugs;
+
+
+IHost host = Host
+    .CreateDefaultBuilder()
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<IFileService, FileService>();
+        services.AddSingleton<IProductService, ProductService>();
+
+        services.AddTransient<ProductDialog>();
+        services.AddTransient<MenuDialog>();
+    })
+    .Build();
+    
+var menu = host.Services.GetRequiredService<MenuDialog>();
+menu.Run();
