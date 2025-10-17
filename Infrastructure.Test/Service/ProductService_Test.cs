5e0e97f8043f48e1c964d406e5ede3d8739889db
Nima <nima010607@gmail.com>
2025-10-17 14:04:34 +0200

    ProductService_Test created.

---
+++ Infrastructure.Test/Service/ProductService_Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Test.Service
+{
+    internal class ProductService_Test
+    {
+    }
+}
